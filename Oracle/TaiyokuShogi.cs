9627e461b6559bbf076ef16c76e038195265ba40
Ian Bearman <ianb@microsoft.com>
2020-11-03 18:03:36 -0800

    Add project files.

---
+++ Oracle/TaiyokuShogi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+
+namespace Oracle
+{
+    public enum Player
+    {
+        White,
+        Black
+    }
+
+    public class TaiyokuShogi
+    {
+        public const int BoardHeight = 36;
+        public const int BoardWidth = 36;
+
+        private readonly (Player, PieceIdentity)?[,] _state = new (Player, PieceIdentity)?[BoardHeight, BoardHeight];
+
+        public TaiyokuShogi()
+        {
+            SetInitialBoard();
+        }
+
+        public (Player Player, PieceIdentity Id)? GetPiece(int x, int y) => _state[x, y];
+
+        //   	 	 	 	 	D	 	 	 	 	GB	 	 	 	D	 	 	 	 	 	 	D	 	 	 	GB	 	 	 	 	D	 	 	 	 	 
+        //  P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P	P
+        //  LC	MK	VM	OX	LB	VP	VH	BN	DH	DK	SE	HF	EL	SP	VL	TG	SB	LD	DG	SB	TG	VL	SP	EL	HF	SE	DK	DH	BN	VH	VP	LB	OX	VM	MK	RC
+        //  CH	SL	VR	WN	RE	M	SD	HS	GN	OS	EA	BS	SG	LP	T	BE	I	GM	GE	I	BE	T	LP	SG	BS	EA	OS	GN	HS	SD	M	RE	WN	VR	SL	CH
+        //  EC	BL	EB	HO	OW	CM	CS	SW	BM	BT	OC	SF	BB	OR	SQ	SN	RD	LI	FE	RD	SN	SQ	OR	BB	SF	OC	BT	BM	SW	CS	CM	OW	HO	EB	VI	EC
+        //  TC	VW	SX	DO	FH	VB	AB	EW	LH	CK	OM	CC	WS	ES	VS	NT	TF	RM	MT	TF	NT	VS	SU	NB	CC	OM	CK	LH	EW	AB	VB	FH	DO	SX	VW	TC
+        //  WC	WH	HD	SM	PR	WB	FL	EG	FD	PS	FY	ST	BI	WG	F	KR	CA	GT	LL	HM	PH	F	WG	BI	ST	FY	PS	FD	EG	FL	WB	PR	SM	HD	WH	WC
+        //  CI	CE	B	R	WF	FC	MF	VT	SO	LS	CL	CR	RH	HE	VO	GD	GO	DV	DS	GO	GD	VO	HE	RH	CR	CL	LS	SO	VT	MF	FC	WF	R	B	CE	CI
+        //  SV	VE	N	PI	CG	PG	H	O	CN	SA	SR	GL	LN	CT	GS	VD	WL	GG	VG	WL	VD	GS	CT	LN	GL	SR	SA	CN	O	H	PG	CG	PI	N	VE	SV
+        //  GC	SI	RN	RW	BG	RO	LT	LE	BO	WD	FP	RB	OK	PC	WA	FI	C	KM	PM	C	FI	WA	PC	OK	RB	FP	WD	BO	RI	TT	RO	BG	RW	RN	SI	GC
+        //  RV	WE	TD	FS	CO	RA	FO	MS	RP	RU	SS	GR	RT	BA	BD	WR	S	NK	DE	S	GU	YA	BA	RT	GR	SS	RU	RP	MS	FO	RA	CO	FS	TD	FG	RV
+        //  L	TS	RR	W	DM	ME	LO	BC	HR	FR	ED	CD	FT	Q	RS	LG	G	K	CP	G	RG	RS	Q	FT	WO	ED	FR	HR	BC	LO	ME	DM	W	RR	WT	L
+        private void SetInitialBoard()
+        {
+            _state[0, 35] = (Player.White, PieceIdentity.Lance);
+            _state[1, 35] = (Player.White, PieceIdentity.TurtleSnake);
+            _state[2, 35] = (Player.White, PieceIdentity.RunningRabbit);
+            _state[3, 35] = (Player.White, PieceIdentity.Whale);
+            _state[4, 35] = (Player.White, PieceIdentity.FireDemon);
+            _state[5, 35] = (Player.White, PieceIdentity.LeftMountainEagle);
+            _state[6, 35] = (Player.White, PieceIdentity.LongNosedGoblin);
+            _state[0, 34] = (Player.White, PieceIdentity.ReverseChariot);
+        }
+    }
+}
---
+++ WPF_UI/Board.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Oracle;
+
+namespace WPF_UI
+{
+    /// <summary>
+    /// Interaction logic for board.xaml
+    /// </summary>
+    public partial class Board : UserControl
+    {
+        private TaiyokuShogi Game;
+
+        public int BoardWidth { get => TaiyokuShogi.BoardWidth; }
+        public int BoardHeight { get => TaiyokuShogi.BoardHeight; }
+
+        public double SpaceWidth { get => ActualWidth / BoardWidth; }
+
+        public double SpaceHeight { get => ActualHeight / BoardHeight; }
+
+        public Board()
+        {
+            InitializeComponent();
+        }
+
+        public void SetGame(TaiyokuShogi game) => Game = game;
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            var pos = e.GetPosition(this);
+
+            int x = (int)(pos.X / SpaceWidth);
+            int y = (int)(pos.Y / SpaceHeight);
+
+            ToolTip = null;
+            var piece = Game.GetPiece(x, y);
+            if (piece != null)
+            {
+                ToolTip =
+                    $"{Pieces.Name(piece.Value.Id)}\n" + 
+                    $"{Pieces.Kanji(piece.Value.Id)} ({Pieces.Romanji(piece.Value.Id)})";
+            }
+
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (Game != null)
+            {
+                DrawBoard(dc);
+                DrawPieces(dc);
+            }
+
+            base.OnRender(dc);
+        }
+
+        private void DrawBoard(DrawingContext dc)
+        {
+            var pen = new Pen(new SolidColorBrush(Colors.Black), 1.0);
+
+            for (int i = 0; i < BoardWidth + 1; ++i)
+            {
+                dc.DrawLine(pen, new Point(ActualWidth / BoardWidth * i, 0.0), new Point(ActualWidth / BoardWidth * i, ActualHeight));
+            }
+
+            for (int i = 0; i < BoardHeight + 1; ++i)
+            {
+                dc.DrawLine(pen, new Point(0.0, ActualHeight / BoardHeight * i), new Point(ActualWidth, ActualHeight / BoardHeight * i));
+            }
+        }
+
+        private void DrawPieces(DrawingContext dc)
+        {
+            for (int i = 0; i < BoardWidth; ++i)
+            {
+                for (int j = 0; j < BoardHeight; ++j)
+                {
+                    var piece = Game.GetPiece(i, j);
+
+                    if (piece != null)
+                    {
+                        DrawPiece(dc, (i, j), piece.Value.Id, piece?.Player == Player.Black);
+                    }
+                }
+            }
+        }
+
+        private void DrawPiece(DrawingContext dc, (int X, int Y) loc, PieceIdentity id, bool rotate)
+        {
+            var pen = new Pen(new SolidColorBrush(Colors.Black), 0.5);
+
+            var spaceWidth = (ActualWidth / BoardWidth);
+            var spaceHeight = (ActualHeight / BoardHeight);
+
+            var border = spaceWidth * 0.05; // 5% border
+
+            var pieceWidth = (spaceWidth - (2 * border)) * 0.7; // narrow piece
+            var pieceHeight = (spaceHeight - (2 * border));
+
+            var upperWidth = pieceWidth * 0.2;
+            var upperHeight = pieceHeight * 0.3;
+
+            var upperLeft = new Point((pieceWidth - upperWidth) / 2, upperHeight);
+            var upperRight = new Point(spaceWidth - upperLeft.X, upperHeight);
+            var upperMid = new Point(spaceWidth / 2, border);
+
+            var lowerLeft = new Point((spaceWidth - pieceWidth) / 2, spaceHeight - border);
+            var lowerRight = new Point(spaceWidth - lowerLeft.X, spaceHeight - border);
+
+            dc.PushTransform(new TranslateTransform(spaceWidth * loc.X, spaceHeight * loc.Y));
+            dc.PushTransform(new RotateTransform(rotate ? 180 : 0, spaceWidth / 2, spaceHeight / 2));
+            dc.DrawLine(pen, upperLeft, upperMid);     //  /
+            dc.DrawLine(pen, upperMid, upperRight);    //    \
+            dc.DrawLine(pen, upperRight, lowerRight);  //    |
+            dc.DrawLine(pen, lowerRight, lowerLeft);   //    _
+            dc.DrawLine(pen, lowerLeft, upperLeft);    // |
+
+            var verticalKanji = string.Join("\n", Pieces.Kanji(id).EnumerateRunes());
+
+            // Create the initial formatted text string.
+            var formattedText = new FormattedText(
+                verticalKanji,
+                CultureInfo.GetCultureInfo("jp-jp"),
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                spaceHeight * 0.3,
+                Brushes.Black,
+                1.25);
+
+
+            var textStart = new Point(spaceWidth / 2 - (spaceWidth * 0.1), upperHeight - (upperHeight * 0.25));
+
+            dc.DrawText(formattedText, textStart);
+
+            dc.Pop();
+            dc.Pop();
+        }
+    }
+}
