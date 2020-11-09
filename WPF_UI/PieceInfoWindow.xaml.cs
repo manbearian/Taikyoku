d3d2715c72c1f44bcb954d86f38ef4def1c511f1
Ian Bearman <ianb@microsoft.com>
2020-11-08 17:29:56 -0800

    UI: add support for jumps too tool tip

--- WPF_UI/PieceInfoWindow.xaml.cs
+++ WPF_UI/PieceInfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +25,13 @@ namespace WPF_UI
             InitializeComponent();
         }
 
+
+        // render moves in a grid
+        //     | ○ |  
+        //  ---+---+---
+        //   ─ | ☖| ─
+        //  ---+---+---
+        //   ○ |   | ○
         public void SetPiece(TaiyokuShogi game, PieceIdentity id)
         {
             var moves = game.GetMovement(id);
@@ -35,6 +43,18 @@ namespace WPF_UI
             for (int i = 0; i < moves.StepRange.Length; ++i)
             {
                 maxMoves = Math.Max(maxMoves, moves.StepRange[i] == Movement.Unlimited ? 1 : moves.StepRange[i]);
+            }
+
+            for (int i = 0; i < moves.JumpRange.Length; ++i)
+            {
+                var jumpRange = moves.JumpRange[i].JumpDistances?.Max() + 1;
+                jumpRange += moves.JumpRange[i].RangeAfter == Movement.Unlimited ? 1 : moves.JumpRange[i].RangeAfter;
+                maxMoves = Math.Max(maxMoves, jumpRange ?? 0);
+            }
+
+            if (moves.HookMove.HasValue)
+            {
+                maxMoves = Math.Max(maxMoves, 2);
             }
 
             moveGrid.ColumnDefinitions.Clear();
@@ -50,48 +70,71 @@ namespace WPF_UI
                 moveGrid.RowDefinitions.Add(new RowDefinition());
             }
 
-            // render moves in a grid
-            //     | ○ |  
-            //  ---+---+---
-            //   ─ | ☖| ─
-            //  ---+---+---
-            //   ○ |   | ○
-            var pieceIcon = new TextBlock();
+            var glyphGrid = new TextBlock[gridSize, gridSize];
+
+            for (int i = 0; i < gridSize; ++i)
+            {
+                for (int j = 0; j < gridSize; ++j)
+                {
+                    var glyphBox = new TextBlock
+                    {
+                        Text = "",
+                        FontSize = 12,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center
+                    };
+                    Grid.SetColumn(glyphBox, i);
+                    Grid.SetRow(glyphBox, j);
+                    moveGrid.Children.Add(glyphBox);
+                    glyphGrid[i, j] = glyphBox;
+                }
+            }
+
+            var pieceIcon = glyphGrid[gridSize / 2, gridSize / 2];
             pieceIcon.Text = "☖";
             pieceIcon.FontSize = 14;
-            pieceIcon.HorizontalAlignment = HorizontalAlignment.Center;
-            pieceIcon.VerticalAlignment = VerticalAlignment.Center;
-            Grid.SetColumn(pieceIcon, gridSize / 2);
-            Grid.SetRow(pieceIcon, gridSize / 2);
-            moveGrid.Children.Add(pieceIcon);
 
             for (int direction = 0; direction < moves.StepRange.Length; ++direction)
             {
                 for (int i = 1; i <= Math.Min(moves.StepRange[direction], maxMoves); ++i)
                 {
-                    var moveIcon = new TextBlock();
-                    moveIcon.Text = moves.StepRange[direction] == 0 ? "" : moves.StepRange[direction] < Movement.Unlimited ? "○" : GetFullRangeChar(direction);
-                    moveIcon.FontSize = 12;
-                    moveIcon.HorizontalAlignment = HorizontalAlignment.Center;
-                    moveIcon.VerticalAlignment = VerticalAlignment.Center;
                     var (gridX, gridY) = GetGridPos(gridSize, direction, i);
-                    Grid.SetColumn(moveIcon, gridX);
-                    Grid.SetRow(moveIcon, gridY);
-                    moveGrid.Children.Add(moveIcon);
+                    glyphGrid[gridX, gridY].Text = GetMoveChar(moves.StepRange[direction], direction);
                 }
             }
 
-            static string GetFullRangeChar(int direction) =>
+            for (int direction = 0; direction < moves.JumpRange.Length; ++direction)
+            {
+                var jumpInfo = moves.JumpRange[direction];
+                if (jumpInfo.JumpDistances == null)
+                    continue;
+
+                foreach (var jumpDistance in jumpInfo.JumpDistances)
+                {
+                    var (gridX, gridY) = GetGridPos(gridSize, direction, jumpDistance + 1);
+                    glyphGrid[gridX, gridY].Text = "✬";
+                }
+
+                var maxJumpRange = (jumpInfo.RangeAfter < Movement.Unlimited) ? jumpInfo.RangeAfter + jumpInfo.JumpDistances.Max() + 2 : Movement.Unlimited;
+                for (int i = jumpInfo.JumpDistances.Max() + 2; i <= Math.Min(maxJumpRange, maxMoves); ++i)
+                {
+                    var (gridX, gridY) = GetGridPos(gridSize, direction, i);
+                    glyphGrid[gridX, gridY].Text = GetMoveChar(jumpInfo.RangeAfter, direction);
+                }
+            }
+
+            static string GetMoveChar(int amount, int direction) =>
+                amount < Movement.Unlimited ? "○" :
                 direction switch
                 {
-                    Movement.Up => "|",
-                    Movement.Down => "|",
+                    Movement.Up => "│",
+                    Movement.Down => "│",
                     Movement.Left => "─",
                     Movement.Right => "─",
-                    Movement.UpLeft => "\\",
-                    Movement.UpRight => "/",
-                    Movement.DownLeft => "/",
-                    Movement.DownRight => "\\",
+                    Movement.UpLeft => "╲",
+                    Movement.UpRight => "╱",
+                    Movement.DownLeft => "╱",
+                    Movement.DownRight => "╲",
                     _ => throw new InvalidOperationException()
                 };
 
@@ -106,6 +149,15 @@ namespace WPF_UI
                     Movement.UpRight => (gridSize / 2 + value, gridSize / 2 - value),
                     Movement.DownLeft => (gridSize / 2 - value, gridSize / 2 + value),
                     Movement.DownRight => (gridSize / 2 + value, gridSize / 2 + value),
+
+                    Movement.UpLeftLeft => (gridSize / 2 - value, gridSize / 2 - (value / 2)),
+                    Movement.UpRightRight => (gridSize / 2 + value, gridSize / 2 - (value / 2)),
+                    Movement.DownLeftLeft => (gridSize / 2 - value, gridSize / 2 + (value / 2)),
+                    Movement.DownRightRight => (gridSize / 2 + value, gridSize / 2 + (value / 2)),
+                    Movement.UpUpLeft => (gridSize / 2 - (value / 2), gridSize / 2 - value),
+                    Movement.UpUpRight => (gridSize / 2 + (value / 2), gridSize / 2 - value),
+                    Movement.DownDownLeft => (gridSize / 2 - (value / 2), gridSize / 2 + value),
+                    Movement.DownDownRight => (gridSize / 2 + (value / 2), gridSize / 2 + value),
                     _ => throw new NotSupportedException()
                 };
         }
