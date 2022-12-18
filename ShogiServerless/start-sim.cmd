fc8110face0a14c825301d8daae1c74603e29dc8
Ian Bearman <ianb@microsoft.com>
2022-12-17 21:42:36 -0800

    ensure simulators start in correct directory

--- ShogiServerless/start-sim.cmd
+++ ShogiServerless/start-sim.cmd
@@ -1,2 +1,4 @@
+@setlocal
+@pushd %~dp0
 start "Azure Functions Simulator" asrs-emulator start
 start "Azure Storage Simulator" "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe"
