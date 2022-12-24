616bb827d6a931207add4518fd1bd21fdccb8cad
Ian Bearman <ianb@microsoft.com>
2022-12-24 08:15:25 -0800

    move azure storage simulator files into a directory

--- ShogiServerless/start-sim.cmd
+++ ShogiServerless/start-sim.cmd
@@ -1,4 +1,6 @@
 @setlocal
 @pushd %~dp0
 start "Azure Functions Simulator" asrs-emulator start
+if not exist .azurite md .azurite
+@pushd .azurite
 start "Azure Storage Simulator" "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe"
