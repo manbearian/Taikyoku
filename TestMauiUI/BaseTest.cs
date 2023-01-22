04dab33cc9a6df4e09fc755c8889d7a8ccd7fe98
Ian Bearman <ianb@microsoft.com>
2023-01-22 12:14:30 -0800

    add initial unit test (does nothing but instantiate a view)

---
+++ TestMauiUI/BaseTest.cs
@@ -0,0 +1,44 @@
+﻿
+using CommunityToolkit.Maui.UnitTests.Mocks;
+using System.Globalization;
+
+namespace TestMauiUI;
+
+// Base Test, basic idea comes from CommunityToolkit.Maui.UnitTests.BaseTest
+public abstract class BaseTest : IDisposable
+{
+    readonly CultureInfo defaultCulture, defaultUiCulture;
+
+    bool isDisposed;
+
+    protected BaseTest()
+    {
+        defaultCulture = Thread.CurrentThread.CurrentCulture;
+        defaultUiCulture = Thread.CurrentThread.CurrentUICulture;
+
+        DispatcherProvider.SetCurrent(new MockDispatcherProvider());
+    }
+
+    ~BaseTest() => Dispose(false);
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool isDisposing)
+    {
+        if (isDisposed)
+        {
+            return;
+        }
+
+        Thread.CurrentThread.CurrentCulture = defaultCulture;
+        Thread.CurrentThread.CurrentUICulture = defaultUiCulture;
+        
+        DispatcherProvider.SetCurrent(null);
+
+        isDisposed = true;
+    }
+}
---
+++ TestMauiUI/MyGamesViewTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+using MauiUI;
+
+namespace TestMauiUI;
+
+public class MyGamesViewTests : BaseTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var x = new MyGamesView();
+    }
+}
